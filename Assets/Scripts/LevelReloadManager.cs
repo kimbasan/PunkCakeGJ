2a3b53ee5c8613ce1c5ab7398e5187dcf7df020b
AHancharou <asgnchrv@gmail.com>
2023-04-29 22:13:25 +0300

    Prototype movement

---
+++ Assets/Scripts/LevelReloadManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelReloadManager : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private CloneMovement cloneMovement;
+    [SerializeField] private GameObject clone;
+
+    private void Awake()
+    {
+        PlayerMoves previousMoves = SavingManager.Load();
+        if (previousMoves != null)
+        {
+            cloneMovement.olderMoves = previousMoves;
+            clone.SetActive(true);
+        }
+    }
+    public void ReloadLevel()
+    {
+        SavingManager.Save(new PlayerMoves(playerMovement.movementsQueue));
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
