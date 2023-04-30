ffca65b2e3f4a85ffc1e45a5029160bd9b946bb8
Fetych <artem.petrovich@yandex.by>
2023-04-30 04:17:50 +0300

    Перемещение

--- Assets/Scripts/LevelReloadManager.cs
+++ Assets/Scripts/LevelReloadManager.cs
@@ -3,22 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class LevelReloadManager : MonoBehaviour
 {
-    [SerializeField] private PlayerMovement playerMovement;
-    [SerializeField] private CloneMovement cloneMovement;
-    [SerializeField] private GameObject clone;
+    //[SerializeField] private PlayerMovement playerMovement;
+    //[SerializeField] private CloneMovement cloneMovement;
+    //[SerializeField] private GameObject clone;
 
-    private void Awake()
-    {
-        PlayerMoves previousMoves = SavingManager.Load();
-        if (previousMoves != null)
-        {
-            cloneMovement.olderMoves = previousMoves;
-            clone.SetActive(true);
-        }
-    }
-    public void ReloadLevel()
-    {
-        SavingManager.Save(new PlayerMoves(playerMovement.movementsQueue));
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
+    //private void Awake()
+    //{
+    //    PlayerMoves previousMoves = SavingManager.Load();
+    //    if (previousMoves != null)
+    //    {
+    //        cloneMovement.olderMoves = previousMoves;
+    //        clone.SetActive(true);
+    //    }
+    //}
+    //public void ReloadLevel()
+    //{
+    //    SavingManager.Save(new PlayerMoves(playerMovement.movementsQueue));
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    //}
 }
