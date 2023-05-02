47f58a2ce0b8b2bfcb89904128c45ee4e152ac92
Fetych <artem.petrovich@yandex.by>
2023-05-02 22:41:09 +0300

    Логика поиска объектов и включение подсветки

---
+++ Assets/Scripts/AssignmentOfKeyInteraction.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AssignmentOfKeyInteraction : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI[] TextAction;//текст с буквами для взаимодействия
+    [SerializeField] private Outline Outline;//б=подсветка
+
+    private void Start()
+    {
+        TurningOffText();
+        Outline = GetComponent<Outline>();
+        Outline.enabled = false;//выключает подстветку
+    }
+    public void TurningOffText()//выключает текст
+    {
+        for (int i = 0; i < TextAction.Length; i++)
+        {
+            TextAction[i].enabled = false;
+        }
+    }
+    public void OnText(int Index)//включает текст по индексу из Move
+    {
+        TextAction[Index].enabled = true;
+        Outline.enabled = true;
+    }
+    public void OffText(int Index)//выключает текст по индексу из Move
+    {
+        TextAction[Index].enabled = false;
+        Outline.enabled = false;
+    }
+}
