0cc039e94f8f5741f3a1f84f2dbd68e10b4c479d
Fetych <artem.petrovich@yandex.by>
2023-05-03 23:03:07 +0300

    Скрипт на звук

---
+++ Assets/Soun.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soun : MonoBehaviour
+{
+    public void Sound()
+    {
+        // AudioSuorce
+        // Clip
+        // AudioSuorce.PLayOneShot(Clip);
+        Debug.Log(1);
+    }
+}
