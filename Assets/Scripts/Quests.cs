912efac2e923bd90ff7685500485629523bf8b0b
Fetych <artem.petrovich@yandex.by>
2023-05-03 23:54:49 +0300

    Обучающие квесты

--- Assets/Scripts/Quests.cs
+++ Assets/Scripts/Quests.cs
@@ -6,10 +6,10 @@ using TMPro;
 
 public class Quests : MonoBehaviour
 {
-    //[SerializeField] private TextMeshProUGUI[] MainQuest;
-    [SerializeField] private GameObject MainQuest;// FindTheKeycardQuest, CollectGlitterQuest, NeutralizeGuardQuest;
+    [SerializeField] private GameObject MainQuest;
     [SerializeField] private GameObject[] AdditionalQuests, EducationalQuests, ProgressOfTheCompletedTask;
     [SerializeField] private bool[] AdditionalTasksReceivedBool, CompletingTheQuest;
+    private int IndexEducation = 0;
 
     private void Start()
     {
@@ -22,22 +22,29 @@ public class Quests : MonoBehaviour
                 AdditionalQuests[i].SetActive(true);
             }
         }
-        for(int i = 0; i < EducationalQuests.Length; i++)
+        for(int i = 1; i < EducationalQuests.Length; i++)
         {
             EducationalQuests[i].SetActive(false);
         }
-
-        //for (int i = 0; i < NeutralizeGuardQuest.Length; i++)
-        //{
-        //    NeutralizeGuardQuest[i].SetActive(false);
-        //}
-        //MainQuest[0].SetActive(true);
     }
     public void CheckProgressAdditionalQuest(int IndexQuest)
     {
         if (CompletingTheQuest[IndexQuest])
         {
             ProgressOfTheCompletedTask[IndexQuest].SetActive(true);
+        }
+    }
+    public void CheckEducationQuest()
+    {
+        if(IndexEducation < EducationalQuests.Length - 1)
+        {
+            EducationalQuests[IndexEducation].SetActive(false);
+            IndexEducation++;
+            EducationalQuests[IndexEducation].SetActive(true);
+        }
+        else if (IndexEducation < EducationalQuests.Length)
+        {
+            EducationalQuests[IndexEducation].SetActive(false);
         }
     }
 }
