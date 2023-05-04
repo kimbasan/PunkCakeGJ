19b42a87ec8b9dd5b0bdc7a46b101328f8fdddeb
AHancharou <asgnchrv@gmail.com>
2023-05-04 16:52:29 +0300

    Fix

--- Assets/Scripts/ObjectDefinition.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ObjectDefinition : MonoBehaviour
-{
-    [SerializeField] private string[] NameObject = new string[3] { "Блестяшки", "Ключ Карта", "Двери"};
-    public int IndexObject;
-}
