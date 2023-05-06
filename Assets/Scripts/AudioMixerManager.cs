1850ab65ea013e2d078b828fd750552d1ebdf041
Fetych <artem.petrovich@yandex.by>
2023-05-06 15:34:53 +0300

    преф

--- Assets/Scripts/AudioMixerManager.cs
+++ Assets/Scripts/AudioMixerManager.cs
@@ -5,21 +5,21 @@ using UnityEngine.Audio;
 public class AudioMixerManager : MonoBehaviour
 {
     [SerializeField] AudioMixer Mixer;
-    [SerializeField] Slider MusicSlider;//, SFXSlider;
+    [SerializeField] Slider MusicSlider, SFXSlider;
 
     const string Mixer_Music = "MusicVolume";
-    //const string Mixer_SFX = "SFXVolume";
+    const string Mixer_SFX = "SFXVolume";
 
     private void Awake()
     {
         MusicSlider.onValueChanged.AddListener(MusicVolume);
-        //SFXSlider.onValueChanged.AddListener(SFXVolume);
+        SFXSlider.onValueChanged.AddListener(SFXVolume);
     }
 
     private void Start()
     {
         MusicSlider.value = PlayerPrefs.GetFloat("Mixer_Music", 1f);
-        //SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume", 1f);
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume", 1f);
     }
 
     void MusicVolume(float Value)
@@ -28,9 +28,9 @@ public class AudioMixerManager : MonoBehaviour
         PlayerPrefs.SetFloat("Mixer_Music", Value);
     }
 
-    //void SFXVolume(float Value)
-    //{
-    //    Mixer.SetFloat(Mixer_SFX, Mathf.Log10(Value) * 20);
-    //    PlayerPrefs.SetFloat("SFXVolume", Value);
-    //}
+    void SFXVolume(float Value)
+    {
+        Mixer.SetFloat(Mixer_SFX, Mathf.Log10(Value) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", Value);
+    }
 }
