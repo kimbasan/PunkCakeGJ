a93acf77c74b100be7da166156e5bd2745b5fa85
Fetych <artem.petrovich@yandex.by>
2023-05-06 15:02:28 +0300

    AudioMixer

--- Assets/Scripts/AudioMixerManager.cs
+++ Assets/Scripts/AudioMixerManager.cs
@@ -5,23 +5,32 @@ using UnityEngine.Audio;
 public class AudioMixerManager : MonoBehaviour
 {
     [SerializeField] AudioMixer Mixer;
-    [SerializeField] Slider MusicSlider;
+    [SerializeField] Slider MusicSlider, SFXSlider;
 
     const string Mixer_Music = "MusicVolume";
+    const string Mixer_SFX = "SFXVolume";
 
     private void Awake()
     {
         MusicSlider.onValueChanged.AddListener(MusicVolume);
+        SFXSlider.onValueChanged.AddListener(SFXVolume);
     }
 
     private void Start()
     {
         MusicSlider.value = PlayerPrefs.GetFloat("Mixer_Music", 1f);
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume", 1f);
     }
 
     void MusicVolume(float Value)
     {
         Mixer.SetFloat(Mixer_Music, Mathf.Log10(Value) * 20);
         PlayerPrefs.SetFloat("Mixer_Music", Value);
+    }
+
+    void SFXVolume(float Value)
+    {
+        Mixer.SetFloat(Mixer_SFX, Mathf.Log10(Value) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", Value);
     }
 }
